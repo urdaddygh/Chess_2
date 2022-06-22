c5a4498007cc187d37a64b515810d59cb3d19c17
urdaddygh <aziret.2000@bk.ru>
2022-06-22 12:46:15 +0600

    first commit

---
+++ build/precache-manifest.01eaa9d09ffdc3d657b56ed9359eae1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2a0499154ba93a6d5a89f72c192053",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4376b83dc6daab5d991c",
+    "url": "/static/js/2.eaa167fc.chunk.js"
+  },
+  {
+    "revision": "b0fdf2a9c4578117c6dd68ce66fcd887",
+    "url": "/static/js/2.eaa167fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8849be6d88b1d25dc6d",
+    "url": "/static/js/3.369e67f9.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/static/js/3.369e67f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c09634c183d66442c4",
+    "url": "/static/js/main.63d23cf2.chunk.js"
+  },
+  {
+    "revision": "d1f05ea41ed905263658",
+    "url": "/static/js/runtime-main.e8fb1a38.js"
+  },
+  {
+    "revision": "43e6bf62035e63a19f956ec3fa90af6d",
+    "url": "/static/media/moveSoundEffect.43e6bf62.mp3"
+  }
+]);
